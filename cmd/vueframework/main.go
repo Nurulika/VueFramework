1fe287b3b075cec3027930f84435492276aa63ee
Nuru <ceruks@eteb.me>
2025-10-03 18:54:08 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/vueframework/main.go
@@ -0,0 +1,23 @@
+// cmd/vueframework/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "vueframework/internal/vueframework"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := vueframework.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
